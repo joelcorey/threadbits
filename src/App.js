84e9cf8c1d0c1505bb46a3ac23aa9e142feb8897
Joel Corey <joelcorey@fastmail.com>
2021-03-09 08:44:04 -0800

    resize listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Button from './component/stitches/button/button';
@@ -8,10 +8,29 @@ import Drag from './component/Drag/Drag';
 
 function App() {
 
+  let [dimensions, setDimensions] = useState({})
+
+  // function getDimensions() {
+  //   let body = document.querySelector('html');
+  //   setDimensions({ width: body.clientWidth, height: body.clientHeight });
+  // }
+
+  function getDimensions() {
+    let body = document.querySelector('html');
+    setDimensions({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  useEffect(() => {
+    getDimensions();
+
+    window.addEventListener('resize', getDimensions);
+  }, [])
+
   return (
     <Wrapper>
       <Menubar />
       <Button onClick={() => {alert('hello!')}}>hi</Button>
+      <p>{dimensions.width} {dimensions.height}</p>
       <Drag />
     </Wrapper>
   );
--- src/component/Drag/Drag.js
+++ src/component/Drag/Drag.js
@@ -4,9 +4,31 @@ import Draggable from 'react-draggable';
 function Drag(props) {
 
   return (
-    <Draggable>
-      <div>
-        hi
+    <Draggable
+      handle=".handle"
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: 'solid black 1px',
+          maxWidth: '20%',
+        }}
+      >
+        <div
+          className="handle"
+          style={{
+            display: '',
+            height: '2vh',
+            backgroundColor: '#5EA9BE',
+            fontSize: '10px'
+          }}
+        >
+          Source
+        </div>
+        <div>
+          hi
+        </div>
       </div>
     </Draggable>
   );
